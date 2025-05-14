66b75d66ec16db03bba833a30d59129674692adc
nmarkenley1 <markenley.nicolas@pursuit.org>
2025-05-14 16:50:23 -0400

    upload from file commit

---
+++ index.js
@@ -0,0 +1,33 @@
+let sumHel = document.getElementById("sumH-el")
+let sumGel = document.getElementById("sumG-el")
+let scoreH = 0; 
+let scoreG = 0; 
+  function add1H() {
+    scoreH += 1
+    sumHel.textContent= scoreH
+}
+  function add2H() {
+    scoreH += 2
+    sumHel.textContent= scoreH
+}
+ function add3H() {
+    scoreH += 3
+    sumHel.textContent= scoreH
+}
+
+
+   function add1G() {
+        scoreG += 1;
+        sumGel.textContent = scoreG;
+    }
+
+    function add2G() {
+        scoreG += 2;
+        sumGel.textContent = scoreG;
+    }
+
+    function add3G() {
+        scoreG += 3;
+        sumGel.textContent = scoreG;
+        }
+    
